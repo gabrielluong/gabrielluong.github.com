cee9ed906512964eb04c9cfce0ea4e08bccef672
Gabriel Luong <gabriel.luong@gmail.com>
2013-07-25 16:12:30 -0400

    Added orion gitdiff plugin

---
+++ orion/plugin/gitdiff/README.md
@@ -0,0 +1,2 @@
+orion-git-diff-plugin
+=====================
---
+++ orion/plugin/gitdiff/gitDiffPlugin.js
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*global define URL window*/
+define(["orion/plugin", "orion/Deferred", "orion/xhr", "domReady!"], function(PluginProvider, Deferred, xhr) {
+    var provider = new PluginProvider({
+        name: "Git Diff Editor Annotation Plugin",
+        version: "1.0",
+        description: "Integrates with Eclipse Orion editor to provide git diff annotation.",
+        license: "Eclipse Distribution License",
+        website: "https://github.com/gabrielluong/orion-git-diff-plugin"
+    });
+    
+	provider.registerService("orion.core.diff", {
+		getDiffContent: function(diffURI){	
+			var url = new URL(diffURI, window.location);
+			url.query.set("parts", "diff");
+			return xhr("GET", url.href, {
+				headers: {
+					"Orion-Version": "1"
+				},
+				timeout: 15000
+			}).then(function(xhrResult) {
+				alert(xhrResult.responseText);
+				return xhrResult.responseText;
+			});
+		},			
+		getDiffFileURI: function(diffURI){
+			var url = new URL(diffURI, window.location);
+			url.query.set("parts", "uris");
+			return xhr("GET", url.href, {
+				headers: {
+					"Orion-Version": "1"
+				},
+				timeout: 15000
+			}).then(function(xhrResult) {
+				return JSON.parse(xhrResult.responseText);
+			});
+		}
+	}, {
+		name: "Git Diff",
+		key: ["g", false, false, true] //alt+g
+	});
+    provider.connect();
+});
